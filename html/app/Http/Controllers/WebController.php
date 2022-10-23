d63610067db05f02c24423a4ce9f96d631f81cd2
KinoshitaShinichi <shiki5544@gmail.com>
2022-10-23 14:31:54 +0900

    おすすめ商品実装

--- html/app/Http/Controllers/WebController.php
+++ html/app/Http/Controllers/WebController.php
@@ -16,7 +16,8 @@ class WebController extends Controller
         $major_category_names = Category::pluck('major_category_name')->unique();
 
         $recently_products = Product::orderBy('created_at', 'desc')->take(4)->get();
+        $recommend_products = Product::where('recommend_flag', true)->take(3)->get();
 
-        return view('web.index', compact('major_category_names', 'categories', 'recently_products'));
+        return view('web.index', compact('major_category_names', 'categories', 'recently_products', 'recommend_products'));
     }
 }
