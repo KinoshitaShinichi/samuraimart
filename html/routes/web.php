f94f3756b323a45322fb2fb6e750322bf332603a
KinoshitaShinichi <shiki5544@gmail.com>
2022-10-23 12:18:42 +0900

    管理者画面実装(7)まで

--- html/routes/web.php
+++ html/routes/web.php
@@ -42,8 +42,9 @@ Route::post('products/{product}/reviews', 'ReviewController@store');
 // ↓お気に入り画面ルート
 Route::get('products/{product}/favorite', 'ProductController@favorite')->name('products.favorite');
 
-// ↓商品登録や画面表示ルート(まとめている)
-Route::resource('products', 'ProductController');
+// ↓
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/{product}', 'ProductController@show')->name('products.show');
 
 // ログイン機能へのルート
 Auth::routes(['verify' => true]);
@@ -59,4 +60,7 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
     Route::get('login', 'Dashboard\Auth\LoginController@showLoginForm')->name('login');
     Route::post('login', 'Dashboard\Auth\LoginController@login')->name('login');
     Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
+    Route::resource('major_categories', 'Dashboard\MajorCategoryController')->middleware('auth:admins');
+    Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
+    Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
 });
